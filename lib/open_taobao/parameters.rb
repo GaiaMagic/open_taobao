b66016f99b8eb81970129bab56cf6d6d4d645be1
ChenWeican <chenweican@gmail.com>
2015-05-05 19:00:43 +0800

    添加参数

--- lib/open_taobao/parameters.rb
+++ lib/open_taobao/parameters.rb
@@ -2,7 +2,7 @@ module OpenTaobao
   PARAMETERS = {
     "taobao.item.add"=>{
       "required"=>["num", "price", "type", "stuff_status", "title", "desc", "cid", "location.state", "location.city"], 
-      "optional"=>["outer_id", "locality_life.choose_logis", "locality_life.merchant", "locality_life.expirydate", "image"]}, 
+      "optional"=>["outer_id", "locality_life.choose_logis", "locality_life.merchant", "locality_life.expirydate", "image", "approve_status"]}, 
     "taobao.item.sku.add"=>{
       "required"=>["num_iid", "properties", "quantity", "price"], 
       "optional"=>["outer_id", "item_price", "lang", "spec_id", "sku_hd_length", "sku_hd_height", "sku_hd_lamp_quantity", "ignorewarning"]}, 
